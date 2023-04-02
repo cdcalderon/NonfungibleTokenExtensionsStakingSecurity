257220cdc58cd32f4e76305233416344dc7265d9
carlos calderon <cdcalderon@gmail.com>
2023-04-02 18:35:35 -0500

    add events and balanceOf implementation

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -7,15 +7,30 @@ contract SimpleNFT {
     /* Type declarations */
     using Strings for uint256;
 
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId
+    );
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool _approved
+    );
+
     mapping(uint256 => address) private _owners;
     //mapping(msg.sender => mapping(addressToApprove-opensea => bool))
     mapping(address => mapping(address => bool)) private _operators;
+    mapping(address => uint256) private _balances;
+
     string baseURL = "https://example.com/images/";
 
     function mint(uint256 _tokenId) external {
         require(_owners[_tokenId] == address(0), "already minted");
         require(_tokenId < 100, "_tokenId too large");
+        emit Transfer(address(0), msg.sender, _tokenId);
         _owners[_tokenId] = msg.sender;
+        _balances[msg.sender] += 1;
     }
 
     function ownerOf(uint256 _tokenId) external view returns (address) {
@@ -35,8 +50,11 @@ contract SimpleNFT {
             msg.sender == _from || _operators[_from][msg.sender],
             "require to be the owner"
         );
+        emit Transfer(_from, _to, _tokenId);
         _operators[_from][msg.sender] = false;
         _owners[_tokenId] = _to;
+        _balances[_from] -= 1;
+        _balances[_to] += 1;
     }
 
     function tokenURI(uint256 _tokenId) external view returns (string memory) {
@@ -46,6 +64,7 @@ contract SimpleNFT {
 
     function setApprovalForAll(address _operator, bool _approved) external {
         _operators[msg.sender][_operator] = _approved;
+        emit ApprovalForAll(msg.sender, _operator, _approved);
     }
 
     function isApprovedForAll(
@@ -53,5 +72,10 @@ contract SimpleNFT {
         address _operator
     ) external view returns (bool) {
         return _operators[_owner][_operator];
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        require(_owner != address(0), "zero address");
+        return _balances[_owner];
     }
 }
