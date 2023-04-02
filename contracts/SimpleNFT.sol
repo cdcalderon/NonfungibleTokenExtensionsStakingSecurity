e07993d07ab0264f2a9c2c9fb492e8be0c9356bc
carlos calderon <cdcalderon@gmail.com>
2023-04-02 18:11:16 -0500

    implement setApprovalForAll

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -8,6 +8,8 @@ contract SimpleNFT {
     using Strings for uint256;
 
     mapping(uint256 => address) private _owners;
+    //mapping(msg.sender => mapping(addressToApprove-opensea => bool))
+    mapping(address => mapping(address => bool)) private _operators;
     string baseURL = "https://example.com/images/";
 
     function mint(uint256 _tokenId) external {
@@ -28,13 +30,28 @@ contract SimpleNFT {
     ) external payable {
         require(_owners[_tokenId] != address(0), "token does not exist");
         require(_owners[_tokenId] == _from, "cannot transfer from"); // check that _from is the owner of token
-        require(msg.sender == _from, "require to be the owner");
-
+        //_operators[_from][msg.sender]  check that .msg.sender is approved for the from address
+        require(
+            msg.sender == _from || _operators[_from][msg.sender],
+            "require to be the owner"
+        );
+        _operators[_from][msg.sender] = false;
         _owners[_tokenId] = _to;
     }
 
     function tokenURI(uint256 _tokenId) external view returns (string memory) {
         require(_owners[_tokenId] != address(0), "does not exist");
         return string(abi.encodePacked(baseURL, _tokenId.toString(), ".jpeg"));
+    }
+
+    function setApprovalForAll(address _operator, bool _approved) external {
+        _operators[msg.sender][_operator] = _approved;
+    }
+
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) external view returns (bool) {
+        return _operators[_owner][_operator];
     }
 }
