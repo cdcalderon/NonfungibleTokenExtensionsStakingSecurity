5f306a790ee378461262b1d3e1e1c697a5d3dda4
carlos calderon <cdcalderon@gmail.com>
2023-04-02 19:27:00 -0500

    add Ownable

--- contracts/OpenZeppelinNFT.sol
+++ contracts/OpenZeppelinNFT.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract OpenZeppelinNFT is ERC721 {
+contract OpenZeppelinNFT is ERC721, Ownable {
     uint256 public tokenSupply = 0;
     uint256 public constant MAX_SUPPLY = 5;
 
@@ -14,5 +15,9 @@ contract OpenZeppelinNFT is ERC721 {
         require(tokenSupply < MAX_SUPPLY, "supply used up");
         _mint(msg.sender, tokenSupply);
         tokenSupply++;
+    }
+
+    function withdraw() external onlyOwner {
+        payable(msg.sender).transfer(address(this).balance);
     }
 }
