6733456016bd16de3c351aae3d02c41826b5f1ac
carlos calderon <cdcalderon@gmail.com>
2023-04-02 19:10:03 -0500

    implement mint function

--- contracts/OpenZeppelinNFT.sol
+++ contracts/OpenZeppelinNFT.sol
@@ -5,5 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract OpenZeppelinNFT is ERC721 {
+    uint256 public tokenSupply = 0;
+    uint256 public constant MAX_SUPPLY = 5;
+
     constructor() ERC721("OpenZeppelinNFT", "OPZ") {}
+
+    function mint() external {
+        require(tokenSupply < MAX_SUPPLY, "supply used up");
+        _mint(msg.sender, tokenSupply);
+        tokenSupply++;
+    }
 }
