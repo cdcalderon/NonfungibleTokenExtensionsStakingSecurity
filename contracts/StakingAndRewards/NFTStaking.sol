5f4d9c932d6b89359387dea0650804743cc02263
carlos calderon <cdcalderon@gmail.com>
2023-04-08 10:51:09 -0500

    Implement feedback:  cannot stake two NFTs and if try to stake a second NFT, will lose the first one / if wait a long time to claim rewards.

--- contracts/StakingAndRewards/NFTStaking.sol
+++ contracts/StakingAndRewards/NFTStaking.sol
@@ -14,12 +14,11 @@ contract NFTStaking is IERC721Receiver, ReentrancyGuard {
     uint256 public constant REWARD_INTERVAL = 24 hours;
 
     struct StakeInfo {
-        uint256 tokenId;
+        address owner;
         uint256 lastClaimed;
-        // bool isStaked;
     }
 
-    mapping(address => StakeInfo) public stakers;
+    mapping(uint256 => StakeInfo) public stakers;
 
     constructor(address _erc20Token, address _erc721Token) {
         erc20Token = MyERC20Token(_erc20Token);
@@ -34,30 +33,28 @@ contract NFTStaking is IERC721Receiver, ReentrancyGuard {
     ) external override returns (bytes4) {
         require(msg.sender == address(erc721Token), "Invalid token");
 
-        stakers[from] = StakeInfo(tokenId, block.timestamp);
-        // stakers[from] = StakeInfo(tokenId, block.timestamp, true);
+        stakers[tokenId] = StakeInfo(from, block.timestamp);
 
         return this.onERC721Received.selector;
     }
 
-    function claimRewards() external nonReentrant {
-        StakeInfo storage stake = stakers[msg.sender];
+    function claimRewards(uint256 tokenId) external nonReentrant {
+        StakeInfo storage stake = stakers[tokenId];
+        require(stake.owner == msg.sender, "Not owner of the staked NFT");
         require(
             stake.lastClaimed + REWARD_INTERVAL <= block.timestamp,
             "Claim interval not met"
         );
 
-        stake.lastClaimed = block.timestamp;
+        stake.lastClaimed += REWARD_INTERVAL;
         erc20Token.transfer(msg.sender, REWARD_AMOUNT);
     }
 
-    function unstake() external nonReentrant {
-        StakeInfo storage stake = stakers[msg.sender];
-        require(stake.tokenId != 0, "Not staked");
-        //require(stake.isStaked, "Not staked");
+    function unstake(uint256 tokenId) external nonReentrant {
+        StakeInfo storage stake = stakers[tokenId];
+        require(stake.owner == msg.sender, "Not owner of the staked NFT");
 
-        uint256 tokenId = stake.tokenId;
-        delete stakers[msg.sender];
+        delete stakers[tokenId];
 
         erc721Token.safeTransferFrom(address(this), msg.sender, tokenId);
     }
